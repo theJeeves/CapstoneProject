711bffb83bfec17c820b04794dac6da377787b90
Nayan Patel <nayanp@uci.edu>
2016-10-13 15:14:28 -0700

    more machine gun stuff 'fixed'

--- Capstone Project/Assets/Scripts/Actions/MainCharacter/Weapons/MachineGun.cs
+++ Capstone Project/Assets/Scripts/Actions/MainCharacter/Weapons/MachineGun.cs
@@ -23,39 +23,46 @@ public class MachineGun : AbstractGun {
             if (_controller.AimDirection.Down) {
                 if (_yBlowBack <= _blowBack && _body2d.velocity.y < 0)
                 {
-                    float multiplier = 1.3f*(_body2d.velocity.y / _yBlowBack);
+                    float multiplier = 1.3f * (_body2d.velocity.y / _yBlowBack);
                     //float multiplier = _yBlowBack > 0 ? Mathf.Abs(_body2d.velocity.y) / _yBlowBack : 1;
                     _yBlowBack += Mathf.Abs(_blowBack * multiplier);
-                }
 
-                if (_controller.AimDirection.Right) {
-                    if (_xBlowBack >= _blowBack * -_xMultiplier)
+
+                    if (_controller.AimDirection.Right)
                     {
-                        _xBlowBack -= _blowBack;
+                        if (_xBlowBack >= _blowBack * -_xMultiplier)
+                        {
+                            _xBlowBack -= _blowBack;
+                        }
+                        //_yBlowBack *= 0.1f;
                     }
-                    _yBlowBack *= 0.1f;
-                }
-                else if (_controller.AimDirection.Left) {
-                    if (_xBlowBack <= _blowBack * _xMultiplier)
+                    else if (_controller.AimDirection.Left)
                     {
-                        _xBlowBack += _blowBack;
+                        if (_xBlowBack <= _blowBack * _xMultiplier)
+                        {
+                            _xBlowBack += _blowBack;
+                        }
+                        //_yBlowBack *= 0.1f;
                     }
-                    _yBlowBack *= 0.1f;
                 }
-                else if (_body2d.velocity.x > 0) {
+                else if (_body2d.velocity.x > 0)
+                {
 
                     _xBlowBack -= 1.0f;
                 }
-                else if (_body2d.velocity.x < 0) {
+                else if (_body2d.velocity.x < 0)
+                {
                     _xBlowBack += 1.0f;
                 }
             }
 
             else if (_controller.AimDirection.Right) {
-                _xBlowBack = -(_blowBack);
+                _xBlowBack += (_blowBack * -0.25f);
+                //_xBlowBack = -(_blowBack);
             }
             else if (_controller.AimDirection.Left) {
-                _xBlowBack = _blowBack;
+                _xBlowBack += (_blowBack * 0.25f);
+                //_xBlowBack = _blowBack;
             }
 
             _body2d.velocity = new Vector2(_xBlowBack, _yBlowBack);
