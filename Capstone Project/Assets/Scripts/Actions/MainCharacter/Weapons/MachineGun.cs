9f81e245e7c031d0dffa0e34eb4dafb322b2f0df
Nayan Patel <nayanp@uci.edu>
2016-10-13 14:21:04 -0700

    machine gun deceleration feels better when falling from large heights

--- Capstone Project/Assets/Scripts/Actions/MainCharacter/Weapons/MachineGun.cs
+++ Capstone Project/Assets/Scripts/Actions/MainCharacter/Weapons/MachineGun.cs
@@ -21,11 +21,11 @@ public class MachineGun : AbstractGun {
             _yBlowBack = _body2d.velocity.y;
 
             if (_controller.AimDirection.Down) {
-                if (_yBlowBack <= _blowBack  && _body2d.velocity.y < 0)
+                if (_yBlowBack <= _blowBack && _body2d.velocity.y < 0)
                 {
-                    Debug.Log(_yBlowBack);
-                    float multiplier = _yBlowBack > 0 ? _body2d.velocity.y / _yBlowBack : 1;
-                    _yBlowBack += _blowBack * multiplier;
+                    float multiplier = 1.3f*(_body2d.velocity.y / _yBlowBack);
+                    //float multiplier = _yBlowBack > 0 ? Mathf.Abs(_body2d.velocity.y) / _yBlowBack : 1;
+                    _yBlowBack += Mathf.Abs(_blowBack * multiplier);
                 }
 
                 if (_controller.AimDirection.Right) {
