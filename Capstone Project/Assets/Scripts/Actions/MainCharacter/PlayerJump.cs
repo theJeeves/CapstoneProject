6b2e2853a087991ad71f566669b0be1fe87ae03f
Daniel <LaraD@uci.edu>
2016-10-15 13:52:19 -0700

    10/15/16 1:51PM
    + Laid out the structure for shooting controls.

--- Capstone Project/Assets/Scripts/Actions/MainCharacter/PlayerJump.cs
+++ Capstone Project/Assets/Scripts/Actions/MainCharacter/PlayerJump.cs
@@ -4,11 +4,25 @@ using System.Collections;
 public class PlayerJump : AbstractPlayerActions {
 
     [SerializeField]
+    private float _jumpHeight;
+    [SerializeField]
     private float _jumpSpeed;
 
+    protected override void OnEnable() {
+        ControllableObject.OnButton += OnButtonDown;
+    }
+
+    protected override void OnDisable() {
+        ControllableObject.OnButton -= OnButtonDown;
+    }
     protected override void OnButtonDown(Buttons button) {
-        if (button == Buttons.Jump && _collisionState.OnSolidGround) {
-            _body2d.velocity = new Vector2(_body2d.velocity.x, _jumpSpeed);
+        //if (button == Buttons.Jump && _collisionState.OnSolidGround) {
+        //    _body2d.velocity = new Vector2(_body2d.velocity.x, _jumpSpeed);
+        //}
+        while (button == Buttons.Jump) {
+            if (button == Buttons.Jump && _collisionState.OnSolidGround) {
+                transform.position = Vector2.MoveTowards(transform.position, transform.position + new Vector3(0, _jumpHeight, 0), _jumpSpeed * Time.deltaTime);
+            }
         }
     }
 }
--- Capstone Project/Assets/Scripts/Actions/MainCharacter/Weapons/AbstractGun.cs
+++ Capstone Project/Assets/Scripts/Actions/MainCharacter/Weapons/AbstractGun.cs
@@ -4,40 +4,119 @@ using System.Collections;
 public abstract class AbstractGun : AbstractPlayerActions {
 
     [SerializeField]
-    protected float _blowBack;
-    [SerializeField]
-    protected float _XYRatio;
+    protected float _recoil;
 
-    protected float _xBlowBack = 0.0f;
-    protected float _yBlowBack = 0.0f;
+    protected float _addVel = 0.35f;
+    protected float _setVel = 0.6f;
 
     protected override void Awake() {
         _controller = GetComponentInParent<ControllableObject>();
         _body2d = GetComponentInParent<Rigidbody2D>();
         _collisionState = GetComponentInParent<PlayerCollisionState>();
+
+        //_down = new Vector2(_body2d.velocity.x, _recoil);
+        //_right = new Vector2(-(_recoil), _body2d.velocity.y);
+        //_left = new Vector2(_recoil, _body2d.velocity.y);
     }
 
     protected override void OnButtonDown(Buttons button) {
 
-        _xBlowBack = _body2d.velocity.x;
-        _yBlowBack = _body2d.velocity.y;
+        //_newVelocity.x = _body2d.velocity.x;
+        //_newVelocity.y = _body2d.velocity.y;
+
+        //if (_controller.AimDirection.Down) {
+
+        //    if (_controller.AimDirection.Right) {
+        //        _body2d.velocity = (_down + _right) * 0.7f;
+        //    }
+        //    else if (_controller.AimDirection.Left) {
+        //        _body2d.velocity = (_down + _left) * 0.7f;
+        //    }
+        //    else {
+        //        _body2d.velocity = _down;
+        //    }
+
+            //if (_controller.AimDirection.Right || _controller.AimDirection.Left) {
+            //    _xRecoil = _controller.AimDirection.Left ? _recoil * 0.5f : _recoil * -0.5f;
+            //    _yRecoil =  _recoil * 0.5f;
+            //}
+        //}
+
+        //else if (_controller.AimDirection.Right) {
+        //    _body2d.velocity = _right;
+        //}
+        //else if (_controller.AimDirection.Left) {
+        //    _body2d.velocity = _left;
+        //}
 
         if (_controller.AimDirection.Down) {
-            _yBlowBack = _blowBack;
 
-            if (_controller.AimDirection.Right || _controller.AimDirection.Left) {
-                _xBlowBack = _controller.AimDirection.Left ? _blowBack : - (_blowBack);
-                _xBlowBack *= _XYRatio;
+            // AIMING DOWN AND RIGHT
+            if (_controller.AimDirection.Right) {
+                
+                // MOVING LEFT
+                if (_body2d.velocity.x < 0) {
+
+                    // FALLING
+                    if (_body2d.velocity.y < 0) {
+                        _body2d.velocity = new Vector2(_body2d.velocity.x + _recoil * -(_addVel), _recoil * _setVel);
+                    }
+
+                    // RISING
+                    else if (_body2d.velocity.y >= 0) {
+                        float temp = 0.0f;
+                        _body2d.velocity = new Vector2(_body2d.velocity.x + _recoil * -_addVel, Mathf.Clamp(temp,_recoil * _addVel,_recoil * 2));
+                    }
+                }
+                else if (_body2d.velocity.x >= 0) {
+                    _body2d.velocity = new Vector2(_recoil * -_setVel, _recoil * _setVel);
+                }
+            }
+            // AIMING DOWN AND LEFT
+            else if (_controller.AimDirection.Left) {
+                if (_body2d.velocity.x > 0) {
+                    if (_body2d.velocity.y < 0) {
+                        _body2d.velocity = new Vector2(_body2d.velocity.x + _recoil * _addVel, _recoil * _setVel);
+                    }
+                    else if (_body2d.velocity.y >= 0) {
+                        _body2d.velocity += new Vector2(_recoil * _addVel, _recoil * _addVel);
+                    }
+                }
+                else if (_body2d.velocity.x <= 0) {
+                    _body2d.velocity = new Vector2(_recoil * _setVel, _recoil * _setVel);
+                }
+            }
+            // AIMING STRAIGHT DOWN
+            else {
+                if (_body2d.velocity.y < 0) {
+                    _body2d.velocity = new Vector2(_body2d.velocity.x, _recoil);
+                }
+                else if (_body2d.velocity.y >= 0) {
+                    float temp1 = _body2d.velocity.y;
+
+                    float temp2 = Mathf.Clamp(_body2d.velocity.y + _recoil, _recoil, _recoil * 2.0f);
+                    _body2d.velocity = new Vector2(_body2d.velocity.x, _recoil);
+                }
             }
         }
-
+        // AIMING RIGHT
         else if (_controller.AimDirection.Right) {
-            _xBlowBack = _blowBack * -1.0f;
+            if (_body2d.velocity.x < 0) {
+                _body2d.velocity += new Vector2(_recoil * -_addVel, 0);
+            }
+            else if (_body2d.velocity.x >= 0) {
+                _body2d.velocity = new Vector2(_recoil * -_setVel, _body2d.velocity.y);
+            }
         }
+        // AIMING LEFT
         else if (_controller.AimDirection.Left) {
-            _xBlowBack = _blowBack;
+            if (_body2d.velocity.x > 0) {
+                _body2d.velocity += new Vector2(_recoil * _addVel, 0);
+            }
+            else if (_body2d.velocity.x <= 0) {
+                _body2d.velocity = new Vector2(_recoil * _setVel, _body2d.velocity.y);
+            }
         }
-
-        _body2d.velocity = new Vector2(_xBlowBack, _yBlowBack);
+        //_body2d.velocity += _newVelocity;
     }
 }
