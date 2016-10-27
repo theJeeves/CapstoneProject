85be74339840c36de64c5b6f2a262fa8c38cbb32
Daniel <LaraD@uci.edu>
2016-10-27 00:53:23 -0700

    10/27/2016 12:52AM
    * Fixed a bug where the Ammo HUD would not initialize until the player
    hit the ground. The issue has been resolved.

--- Capstone Project/Assets/Scripts/UI/DisplayAmmo.cs
+++ Capstone Project/Assets/Scripts/UI/DisplayAmmo.cs
@@ -5,13 +5,9 @@ using UnityEngine.UI;
 public class DisplayAmmo : MonoBehaviour {
 
     private Text _ammoText;
-    private AbstractGun _shotGun;
-    private AbstractGun _machineGun;
 
     private void Awake() {
         _ammoText = GetComponent<Text>();
-        _shotGun = GameObject.FindGameObjectWithTag("Gun").GetComponent<Shotgun>();
-        _machineGun = GameObject.FindGameObjectWithTag("Gun").GetComponent<MachineGun>();
     }
 
     private void OnEnable() {
