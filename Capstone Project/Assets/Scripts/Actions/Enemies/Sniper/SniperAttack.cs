f5645f4e8e2041210ea33a3ebd3d37b7e783de51
Daniel <LaraD@uci.edu>
2016-10-27 19:03:08 -0700

    10/27/2016 7:01PM
    + Added Enemy prefab
    + Enemy script whichs takes a few seconds to lock onto the player and
    fire a projectile at the player
    + Enemy script which fires the projectile at the player
    + Bullet script which spawns the bullet
    + Bullet scripts which moves the bullet toward the player.
    + Bullet scripts which destory the gameObject if it hits the player or
    goes beyond the screen's view

---
+++ Capstone Project/Assets/Scripts/Actions/Enemies/Sniper/SniperAttack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SniperAttack : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _endOfBarrel;
+    [SerializeField]
+    private GameObject _bullet;
+
+	private void OnEnable() {
+        LockOntoPlayer.Fire += Fire;
+    }
+
+    private void OnDisable() {
+        LockOntoPlayer.Fire -= Fire;
+    }
+
+    private void Fire() {
+
+        Instantiate(_bullet, _endOfBarrel.transform.position, Quaternion.identity);
+    }
+}
---
+++ Capstone Project/Assets/Scripts/Actions/Universal/DestroyOffScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOffScreen : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Camera.main.WorldToViewportPoint(transform.position).x < 0 ||
+            Camera.main.WorldToViewportPoint(transform.position).x > 1 ||
+            Camera.main.WorldToViewportPoint(transform.position).y < 0 ||
+            Camera.main.WorldToViewportPoint(transform.position).y > 1) {
+
+            Destroy(gameObject);
+        }
+	}
+}
