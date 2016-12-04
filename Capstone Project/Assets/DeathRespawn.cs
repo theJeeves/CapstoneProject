c164206dcd6757149cd19b384e0bd8248b3e2f1f
theJeeves <larad@uci.edu>
2016-12-04 01:21:45 -0800

    12/04/2016 1:21AM
    + Added a prefab which records to player's last known grounded position
    in case they die from a fall. It will respawn them at that last recorded
    location.
    * Changed a few things in the level such as colliders.

---
+++ Capstone Project/Assets/DeathRespawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathRespawn : MonoBehaviour {
+
+    private Vector3 _respawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D otherGO) {
+
+        if (otherGO.tag == "Player") {
+            otherGO.GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            otherGO.transform.position = _respawnPoint;
+        }
+    }
+
+    private void LastGrounded(Vector3 playerPos) {
+        _respawnPoint = playerPos;
+    }
+}
