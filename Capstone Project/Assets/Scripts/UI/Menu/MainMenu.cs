f9ff72ff00f5917fac90a84fa9288b7c530893da
Nayan Patel <nayanp@uci.edu>
2017-01-22 15:52:50 -0800

    Main menu beta
    Implemented a started main menu UI

---
+++ Capstone Project/Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas _quitMenu;
+
+    [SerializeField]
+    private Button _play;
+
+    [SerializeField]
+    private Button _quit;
+
+    // Use this for initialization
+    void Start()
+    {
+        _quitMenu = _quitMenu.GetComponent<Canvas>();
+        _play = _play.GetComponent<Button>();
+        _quit = _quit.GetComponent<Button>();
+        _quitMenu.enabled = false;
+    }
+
+    public void ExitPress()
+    {
+        _quitMenu.enabled = true;
+        _play.enabled = false;
+        _quit.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        _quitMenu.enabled = false;
+        _play.enabled = true;
+        _quit.enabled = true;
+    }
+
+    public void PlayGame()
+    {
+        Application.LoadLevel(1);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
