825d6801243c1136f78b4534092d785debc2b343
theJeeves <larad@uci.edu>
2017-01-26 03:15:40 -0800

    01/26/2017 3:15AM
    * Github missed this update. Refer to the previous commit for details.

--- Capstone Project/Assets/Scripts/Enviroment/Player Respawn/RespawnPlayer.cs
+++ Capstone Project/Assets/Scripts/Enviroment/Player Respawn/RespawnPlayer.cs
@@ -7,13 +7,36 @@ public class RespawnPlayer : MonoBehaviour {
     private SORespawn _respawnContainer;
     [SerializeField]
     private SOEffects _SOEffect;
+    [SerializeField]
+    private SOWeaponManager _SOWeaponManager;
+
+    private GameObject _otherGo;
+    private bool _respawned = false;
+
+    private void Update() {
+        
+        if (_respawned) {
+
+            _otherGo.GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            _otherGo.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            _otherGo.transform.position = _respawnContainer.respawnPos;
+
+            if (Camera.main.WorldToViewportPoint(_respawnContainer.respawnPos).x > 0.0f &&
+                Camera.main.WorldToViewportPoint(_respawnContainer.respawnPos).x < 1.0f) {
+
+                _otherGo.GetComponent<Rigidbody2D>().gravityScale = 40.0f;
+                _SOEffect.PlayEffect(EffectEnum.PlayerRespawn, _respawnContainer.respawnPos);
+                _SOWeaponManager.Reload();
+                _respawned = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D otherGO) {
 
         if (otherGO.tag == "Player") {
-            _SOEffect.PlayEffect(EffectEnum.PlayerRespawn, _respawnContainer.respawnPos);
-            otherGO.GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
-            otherGO.transform.position = _respawnContainer.respawnPos;
+            _otherGo = otherGO.gameObject;
+            _respawned = true;
         }
     }
 }
