13bafd04d83aa4806ac953c54ab7ed3ecee885c8
Daniel Lara <LaraD@uci.edu>
2017-02-14 14:10:02 -0800

    02/14/2017 2:09PM
    * Fixed the scripted camera bug. The camera now leaves the area without
    getting all messed up.

---
+++ Capstone Project/Assets/ActivateSmartCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateSmartCamera : MonoBehaviour {
+
+    protected GameObject _camera;       // Reference to the main Camera
+
+    private void OnEnable() {
+        _camera = GameObject.FindGameObjectWithTag("SmartCamera");
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherGO) {
+
+        if (otherGO.gameObject.tag == "Player") {
+            _camera.GetComponent<SmartCameraFOV>().enabled = true;
+            _camera.GetComponent<SmartCameraXPosition>().enabled = true;
+            _camera.GetComponent<SmartCameraYPosition>().enabled = true;
+        }
+    }
+
+}
