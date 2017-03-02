95b50977d62384a8e04692b607478c35ae755dfd
Daniel Lara <LaraD@uci.edu>
2017-03-02 10:04:57 -0800

    03/02/2017 10:04AM
    + Added controller support for the new main menu

--- Capstone Project/Assets/ButtonBehavior.cs
+++ Capstone Project/Assets/ButtonBehavior.cs
@@ -3,12 +3,20 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ButtonBehavior : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class ButtonBehavior : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler {
 
     private Text _text;
 
     private void OnEnable() {
         _text = GetComponentInChildren<Text>();
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        _text.color = Color.cyan;
+    }
+
+    public void OnDeselect(BaseEventData eventData) {
+        _text.color = Color.black;
     }
 
     public void OnPointerEnter(PointerEventData data) {
