78f83ea3ff454b9f1455b1f36ffa6cd108e97186
Nayan Patel <nayanp@uci.edu>
2017-03-01 17:08:49 -0800

    messing with laser colors
    laser disco?

--- Capstone Project/Assets/Scripts/Enviroment/rotatingLaser.cs
+++ Capstone Project/Assets/Scripts/Enviroment/rotatingLaser.cs
@@ -20,11 +20,17 @@ public class rotatingLaser : MonoBehaviour
     [SerializeField]
     private LayerMask _whatToHit;
 
+    
+    private Color[] _colorArray = {Color.blue, Color.cyan, Color.green, Color.magenta, Color.magenta, Color.red, Color.yellow};
+    private Color _startColor;
+    private Color _endColor;
 
     // Use this for initialization
     void Start()
     {
         _lr = GetComponentInChildren<LineRenderer>();
+        _startColor = _colorArray[Random.Range(0, 6)];
+        _endColor = _colorArray[Random.Range(0, 6)];
     }
 
     // Update is called once per frame
@@ -36,11 +42,15 @@ public class rotatingLaser : MonoBehaviour
         if (hit.collider != null)
         {
                 Vector2 laserEnd = new Vector2(hit.point.x, hit.point.y);
-                //float _height = Mathf.Abs(hit.point.y - transform.position.y);
-                //float _width = Mathf.Abs(hit.point.x - transform.position.x);
-                //float _distance = Mathf.Sqrt((_height * _height) + (_width * _width));
-                _lr.SetPosition(0, transform.position);
-                _lr.SetPosition(1, laserEnd);
+            //float _height = Mathf.Abs(hit.point.y - transform.position.y);
+            //float _width = Mathf.Abs(hit.point.x - transform.position.x);
+            //float _distance = Mathf.Sqrt((_height * _height) + (_width * _width));
+            _lr.SetWidth(5.0f, 5.0f);
+
+
+            _lr.SetColors(_startColor, _endColor);
+            _lr.SetPosition(0, transform.position);
+             _lr.SetPosition(1, laserEnd);
             if(hit.collider.tag == "Player")
             {
                 GameObject otherGO = GameObject.FindGameObjectWithTag("Player");
