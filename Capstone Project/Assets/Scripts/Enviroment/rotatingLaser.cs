6b1c1b40991175738d3f3ab3aaae0f8de6f32a5c
Nayan Patel <nayanp@uci.edu>
2017-03-01 17:22:28 -0800

    Keeping rotating laser red
    - Other colors won't match our game

--- Capstone Project/Assets/Scripts/Enviroment/rotatingLaser.cs
+++ Capstone Project/Assets/Scripts/Enviroment/rotatingLaser.cs
@@ -20,17 +20,11 @@ public class rotatingLaser : MonoBehaviour
     [SerializeField]
     private LayerMask _whatToHit;
 
-    
-    private Color[] _colorArray = {Color.blue, Color.cyan, Color.green, Color.magenta, Color.magenta, Color.red, Color.yellow};
-    private Color _startColor;
-    private Color _endColor;
 
     // Use this for initialization
     void Start()
     {
         _lr = GetComponentInChildren<LineRenderer>();
-        _startColor = _colorArray[Random.Range(0, 6)];
-        _endColor = _colorArray[Random.Range(0, 6)];
     }
 
     // Update is called once per frame
@@ -45,10 +39,10 @@ public class rotatingLaser : MonoBehaviour
             //float _height = Mathf.Abs(hit.point.y - transform.position.y);
             //float _width = Mathf.Abs(hit.point.x - transform.position.x);
             //float _distance = Mathf.Sqrt((_height * _height) + (_width * _width));
-            _lr.SetWidth(5.0f, 5.0f);
+            _lr.SetWidth(2.5f, 2.5f);
 
 
-            _lr.SetColors(_startColor, _endColor);
+            _lr.SetColors(Color.red, Color.red);
             _lr.SetPosition(0, transform.position);
              _lr.SetPosition(1, laserEnd);
             if(hit.collider.tag == "Player")
