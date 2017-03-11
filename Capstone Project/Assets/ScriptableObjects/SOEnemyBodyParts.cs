0be739063847b4e45912232c805c099ebb7b5524
theJeeves <larad@uci.edu>
2017-03-10 18:17:54 -0800

    03/10/2017 6:17PM
    * All menu systems now remember what the last selected item is, so there are no more situations where there are two highlighted buttons.
    * Using Unity's sprite packer, the all sprites (except for environmental sprites) are organized into appropriate sprite sheets to reduce reading from the disk and improving load times and performance.

--- Capstone Project/Assets/ScriptableObjects/SOEnemyBodyParts.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[CreateAssetMenu(menuName ="SO Enemy Body Parts/New SO Enemy Body Parts")]
-public class SOEnemyBodyParts : ScriptableObject {
-
-    public GameObject[] bodyParts;
-}
