7d5ef95df8f0e789aa97e41fea5c35b4b4106441
Daniel Lara <LaraD@uci.edu>
2017-10-29 00:46:42 -0700

    10/29/2017 12:46PM
    * Fixed the sniper bug

--- Capstone Project/Assets/Scripts/Enemies/Sniper/SniperBulletMovement.cs
+++ Capstone Project/Assets/Scripts/Enemies/Sniper/SniperBulletMovement.cs
@@ -6,15 +6,25 @@ public class SniperBulletMovement : MonoBehaviour {
     [SerializeField]
     private float _speed;
 
+    private Vector3 m_playerPos = Vector3.zero;
+    private bool m_hasFired = false;
+
     private void OnEnable() {
 
         // Get the center of the player based on the collider
-        Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>().bounds.center;
+        m_playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>().bounds.center;
         // Offset where the bullet will go by 7.5 in the y-axis
-        playerPos = new Vector3(playerPos.x, playerPos.y + 7.5f, 0.0f);
+        m_playerPos = new Vector3(m_playerPos.x, m_playerPos.y + 7.5f, 0.0f);
+    }
 
+    private void Update() {
+        
+        if (!m_hasFired) {
 
-        Vector3 unitVect = (playerPos - transform.position).normalized;
-        GetComponent<Rigidbody2D>().AddForce(unitVect * _speed, ForceMode2D.Impulse);
+            m_hasFired = true;
+
+            Vector3 unitVect = (m_playerPos - transform.position).normalized;
+            GetComponent<Rigidbody2D>().AddForce(unitVect * _speed, ForceMode2D.Impulse);
+        }
     }
 }
